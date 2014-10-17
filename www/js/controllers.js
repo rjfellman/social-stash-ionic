f6c8a577c68f7f809da0b796db05fdbf9e745183
Russ Fellman <russ216@gmail.com>
2014-10-16 22:01:18 -0400

    built a create modal for new posts and linked to menu button. controller filled out for its functionality

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -15,4 +15,36 @@ angular.module('starter.controllers', [])
       	id: 2
   	}
   ];
+})
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+  // Form data for the create modal
+  $scope.newData = {};
+
+  // Create the create modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/create.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the create modal to close it
+  $scope.closeCreate = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the create modal
+  $scope.create = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the create action when the user submits the create form
+  $scope.doCreate = function() {
+    console.log('Creating post', $scope.newData);
+    $scope.closeCreate();
+    $localstorage.setObject('post', $scope.newData);
+    
+    var post = $localstorage.getObject('post');
+  	console.log(post);
+  };
 });
